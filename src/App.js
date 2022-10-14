f467a89d4edbef453f2b8bef7c06d5a3009c5f47
csillaout <104451727+csillaout@users.noreply.github.com>
2022-10-14 10:40:37 +0100

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="http://www.csillatoth.co.uk/" target="_blank">Csilla Toth</a> and is {""}
+        <a href="https://github.com/csillaout/my-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
