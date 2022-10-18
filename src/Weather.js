43ae7bcd4acf7af3963b314df57284440c9e30cf
csillaout <104451727+csillaout@users.noreply.github.com>
2022-10-18 13:36:13 +0100

    icon

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: "http://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
