d321f0fd9e8ead10eee1469c1a605aaa39565e31
csillaout <104451727+csillaout@users.noreply.github.com>
2022-10-18 08:57:55 +0100

    html and css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href="http://www.csillatoth.co.uk/" target="_blank">Csilla Toth</a> and is {""}
-        <a href="https://github.com/csillaout/my-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="http://www.csillatoth.co.uk/" target="_blank">
+            Csilla Toth
+          </a>{" "}
+          and is {""}
+          <a href="https://github.com/csillaout/my-app" target="_blank">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
