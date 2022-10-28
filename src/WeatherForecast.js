999a79f64f9e092534cfd022a9a453e201c23735
csillaout <104451727+csillaout@users.noreply.github.com>
2022-10-28 13:43:34 +0100

    5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -7,6 +7,10 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+useEffect(() => {
+setLoaded(false);
+}, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -14,21 +18,25 @@ export default function WeatherForecast(props) {
 
   if (loaded) {
     return (
-      <div className="WeatherForecast">
+      <div className="WeatherForecast mt-4">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
-  
   } else {
-    
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
     let apiKey = "49eff1aaa5656865d0c236db892d316b";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
     return null;
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,30 +2,30 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
-function maxTemperature() {
-    let temperature =  Math.round(props.data.temp.max);
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
     return `${temperature}º`;
-}
-function minTemperature() {
-    let temperature =  Math.round(props.data.temp.min);
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
     return `${temperature}º`;
-}
+  }
 
-function day() {
+  function day() {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
 
-    let days =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
     return days[day];
-}
+  }
 
   return (
     <div>
       <div className="WeatherForecastDay">{day()}</div>
-     
-        <WeatherIcon code={props.data.weather[0].icon} size={36} />
-     
+
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+
       <div className="WeatherForecastTemperatures">
         <span className="WeatherForecastTemperaturesMax">
           {maxTemperature()}
@@ -34,6 +34,6 @@ function day() {
           {minTemperature()}
         </span>
       </div>
- </div>
+    </div>
   );
 }
