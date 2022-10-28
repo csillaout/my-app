322a948196984748de2ef766dc99efb575f8cd7b
csillaout <104451727+csillaout@users.noreply.github.com>
2022-10-28 13:59:49 +0100

    update main temp

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -27,7 +27,7 @@ export default function WeatherTemperature(props) {
   } else {
     let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
-      <div className="WeatherTemperature">
+      <div className="WeatherTemperature d-flex">
         <span className="temperature">{Math.round(fahrenheit)}</span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
